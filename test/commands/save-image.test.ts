4d5381acb6c9a78f5b68052da9fef74aa43ff156
Felipe Santos <felipecassiors@gmail.com>
2020-08-09 16:24:22 -0300

    test(save-image): add case for --no-discard-repository

--- test/commands/save-image.test.ts
+++ test/commands/save-image.test.ts
@@ -2,6 +2,12 @@ import * as execa from "execa";
 
 const bin = `${process.cwd()}/bin/run`;
 
+const runBinary = async (args: string) => {
+  return execa.command(`${bin} ${args}`, {
+    reject: false,
+  });
+};
+
 describe("save-image", () => {
   it("saves an image without tag", async () => {
     const image = "hello-world";
@@ -38,12 +44,29 @@ describe("save-image", () => {
     expect(loadResult.exitCode).toBe(0);
   });
 
+  it("saves an image not discarding repository", async () => {
+    const image = "docker.io/library/hello-world";
+
+    const result = await execa.command(
+      `${bin} save-image ${image} --no-discard-repository`
+    );
+    expect(result.stdout).not.toContain(`tagging with`);
+    expect(result.stdout).toContain(
+      `saving ${image}:latest and compressing to hello-world-latest.tgz`
+    );
+    expect(result.exitCode).toBe(0);
+
+    const loadResult = await execa.command(
+      `docker load -i hello-world-latest.tgz`
+    );
+    expect(loadResult.stdout).toContain(`Loaded image: hello-world:latest`);
+    expect(loadResult.exitCode).toBe(0);
+  });
+
   it("fails when saving an unexisting image", async () => {
     const image = "hello-world:123456";
 
-    const result = await execa.command(`${bin} save-image ${image}`, {
-      reject: false,
-    });
+    const result = await runBinary(`save-image ${image}`);
 
     expect(result.stdout).toContain(
       `Error response from daemon: manifest for ${image} not found: manifest unknown: manifest unknown`
