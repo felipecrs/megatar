8fea6609129ec9a072cb37aa3c66c936890abebc
Felipe Santos <felipecassiors@gmail.com>
2020-09-23 18:17:05 -0300

    chore(devcontainer): wrap postCreateCommand to a script (#65)

---
+++ .devcontainer/post-create.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -euxo pipefail
+
+npm install
+npm link
+
+# Set up autocomplete and reload it on every new shell instance
+zshrc=/etc/zsh/zshrc
+bashrc=/etc/bash.bashrc
+printf '\n%s' 'megatar autocomplete -r &>/dev/null' | sudo tee -a $bashrc $zshrc
+printf '%s\n' "$(megatar autocomplete:script bash)" | sudo tee -a $bashrc
+printf '%s\n' "$(megatar autocomplete:script zsh)" | sudo tee -a $bashrc
+
+cd website/
+npm install
--- .devcontainer/setup-autocomplete.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-set -e
-
-printf '\n%s' 'megatar autocomplete -r &>/dev/null' | tee -a "$HOME/.bashrc" "$HOME/.zshrc"
-printf '%s\n' "$(megatar autocomplete:script bash)" | tee -a "$HOME/.bashrc"
-printf '%s\n' "$(megatar autocomplete:script zsh)" | tee -a "$HOME/.zshrc"
