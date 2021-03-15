261625d84fbd5bcc0d6390fb870cda431c78b782
Felipe Santos <felipecassiors@gmail.com>
2021-03-15 12:30:07 -0300

    test: add utils and remove unneeded await

--- test/commands/chart/list-images.test.ts
+++ test/commands/chart/list-images.test.ts
@@ -1,27 +1,30 @@
-import { getBinPathSync } from "get-bin-path";
-import * as execa from "execa";
 import { stripIndent } from "common-tags";
+import { runCommand } from "../utils";
 
-const bin = getBinPathSync();
 const command = "chart:list-images";
-const runCommand = async (line: string, options?: execa.Options) =>
-  await execa.command(`${bin} ${command} ${line}`, {
-    ...options,
-    reject: false,
-  });
-
-beforeAll(async () => {
-  await execa.command("helm repo add jenkins https://charts.jenkins.io");
-  await execa.command("helm repo update");
-});
 
 describe(command, () => {
-  it("lists the images of a chart", async () => {
-    const result = await runCommand("jenkins/jenkins");
+  it("lists the images of a chart", () => {
+    const result = runCommand(
+      `${command} https://github.com/jenkinsci/helm-charts/releases/download/jenkins-3.2.4/jenkins-3.2.4.tgz`
+    );
     expect(result.stdout).toBe(stripIndent`
       bats/bats:1.2.1
-      jenkins/jenkins:2.263.1
+      jenkins/jenkins:2.277.1
       kiwigrid/k8s-sidecar:0.1.275
+      `);
+    expect(result.exitCode).toBe(0);
+  });
+
+  it("lists the images of multiple charts", () => {
+    const result = runCommand(
+      `${command} https://github.com/jenkinsci/helm-charts/releases/download/jenkins-3.2.4/jenkins-3.2.4.tgz https://releases.rancher.com/server-charts/latest/rancher-2.5.7.tgz`
+    );
+    expect(result.stdout).toBe(stripIndent`
+      bats/bats:1.2.1
+      jenkins/jenkins:2.277.1
+      kiwigrid/k8s-sidecar:0.1.275
+      rancher/rancher:v2.5.7
       `);
     expect(result.exitCode).toBe(0);
   });
--- test/commands/save-image.test.ts
+++ test/commands/save-image.test.ts
@@ -1,89 +1,78 @@
-import { getBinPathSync } from "get-bin-path";
 import * as execa from "execa";
 import * as shell from "shelljs";
 import * as tmp from "tmp";
+import { runCommand } from "./utils";
 
-const bin = getBinPathSync();
-
-const runBinary = async (args: string) =>
-  execa.command(`${bin} ${args}`, {
-    reject: false,
-  });
+const command = "save-image";
 
 beforeAll(() => {
   process.env = Object.assign(process.env, { FORCE_COLOR: 0 });
 });
 
-describe("save-image", () => {
+describe(command, () => {
   let dir: tmp.DirResult;
 
-  beforeEach(async () => {
+  beforeEach(() => {
     dir = tmp.dirSync({ unsafeCleanup: true });
     shell.cd(dir.name);
   });
 
-  afterEach(async () => {
+  afterEach(() => {
     dir.removeCallback();
   });
 
-  it("saves an image without tag", async () => {
+  it("saves an image without tag", () => {
     const image = "hello-world";
 
-    const result = await execa.command(`${bin} save-image ${image}`);
+    const result = runCommand(`${command} ${image}`);
     expect(result.stdout).toContain(
       `saving ${image}:latest and compressing to ${image}-latest.tgz`
     );
     expect(result.exitCode).toBe(0);
 
-    const loadResult = await execa.command(
-      `docker load -i ${image}-latest.tgz`
-    );
+    const loadResult = execa.commandSync(`docker load -i ${image}-latest.tgz`);
     expect(loadResult.stdout).toContain(`Loaded image: ${image}:latest`);
     expect(loadResult.exitCode).toBe(0);
   });
 
-  it("saves an image with a different tag", async () => {
+  it("saves an image with a different tag", () => {
     const image = "hello-world";
     const newTag = "test";
 
-    const result = await execa.command(
-      `${bin} save-image ${image} --new-tag ${newTag}`
-    );
+    const result = runCommand(`${command} ${image} --new-tag ${newTag}`);
     expect(result.stdout).toContain(
       `saving ${image}:${newTag} and compressing to ${image}-${newTag}.tgz`
     );
     expect(result.exitCode).toBe(0);
 
-    const loadResult = await execa.command(
+    const loadResult = execa.commandSync(
       `docker load -i ${image}-${newTag}.tgz`
     );
     expect(loadResult.stdout).toContain(`Loaded image: ${image}:${newTag}`);
     expect(loadResult.exitCode).toBe(0);
   });
 
-  it("saves an image not discarding repository", async () => {
+  it("saves an image not discarding repository", () => {
     const image = "docker.io/library/hello-world";
 
-    const result = await execa.command(
-      `${bin} save-image ${image} --no-discard-repository`
-    );
+    const result = runCommand(`${command} ${image} --no-discard-repository`);
     expect(result.stdout).not.toContain(`tagging with`);
     expect(result.stdout).toContain(
       `saving ${image}:latest and compressing to hello-world-latest.tgz`
     );
     expect(result.exitCode).toBe(0);
 
-    const loadResult = await execa.command(
+    const loadResult = execa.commandSync(
       `docker load -i hello-world-latest.tgz`
     );
     expect(loadResult.stdout).toContain(`Loaded image: hello-world:latest`);
     expect(loadResult.exitCode).toBe(0);
   });
 
-  it("fails when saving an unexisting image", async () => {
+  it("fails when saving an unexisting image", () => {
     const image = "hello-world:123456";
 
-    const result = await runBinary(`save-image ${image}`);
+    const result = runCommand(`${command} ${image}`);
 
     expect(result.stdout).toContain(
       `Error response from daemon: manifest for ${image} not found: manifest unknown: manifest unknown`
